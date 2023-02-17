cef66f4a7e4056eb5e752043ab5a1ec99253f746
David Rodriguez <rodriguezd11@uabc.edu.mx>
2023-02-17 11:24:38 -0800

    agregado gitignore y mas codigo

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const h1 = document.querySelector('h1');
+
+/*
 const p = document.querySelector('p');
 const parrafo = document.querySelector('.parrafo');
 const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+*/
 
+/*
 console.log(input.value);
 
 console.log({
@@ -28,3 +30,20 @@ const img = document.createElement('img');
 img.setAttribute('src', 'https://play-lh.googleusercontent.com/xGWtxUYbGvI5NjlJdtAwBENJBYM9rf1n0KCQZgJoKavOW_ZP0sI-C7oXWD1H_1PVI5k');
 
 pid.append(img);
+
+*/
+
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form')
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btnCalcular = document.querySelector('#btnCalcular');
+const pResult = document.querySelector("#result")
+
+form.addEventListener('submit', btnOnClick);
+
+function btnOnClick(event){
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value
+    pResult.innerText = sumaInputs;
+}
